6b003f4279f0df9d4fe8fedeabfd79206556b923
jchientx <73362100+jchientx@users.noreply.github.com>
2022-01-17 22:47:02 -0600

    Add files via upload

---
+++ ToDo.js
@@ -0,0 +1,101 @@
+const form = document.querySelector("#add-todo");
+const input = document.querySelector("#todo-item");
+const todoList = document.querySelector("#todo-list");
+const todos = document.querySelector("li");
+const colorSection = document.querySelector("#colors");
+const removeBtn = document.createElement("button");
+const completeBtn = document.createElement("button");
+
+//const toggleSwitch = document.querySelector('input[type="checkbox"]');
+// localStorage: When the page refreshes, the todos on the page remain there
+/*
+if (localStorage.getItem("darkModeEnabled")) {
+  document.body.className = "dark";
+  toggleSwitch.checked = true;
+}
+toggleSwitch.addEventListener("click", function (e) {
+  const { checked } = toggleSwitch;
+  if (checked) {
+    localStorage.setItem("darkModeEnabled", true);
+  } else {
+    localStorage.removeItem("darkModeEnabled");
+  }
+  document.body.className = checked ? "dark" : "";
+});
+*/
+
+// retrieve from localStorage
+const savedTodos = JSON.parse(localStorage.getItem("todosAll")) || [];
+for (let i = 0; i < savedTodos.length; i++) {
+  let newTodo = document.createElement("li");
+  newTodo.innerText = savedTodos[i].todoItem;
+  newTodo.isCompleted = savedTodos[i].isCompleted ? true : false;
+  // Why no completed effect?
+  if (newTodo.isCompleted) {
+    newTodo.style.textDecoration = "line-through";
+  }
+  removeBtn.innerText = "Remove";
+  newTodo.appendChild(removeBtn);
+  completeBtn.innerText = "Completed";
+  newTodo.appendChild(completeBtn);
+  todoList.appendChild(newTodo); // Why Remove and Completed button only exists for the last todo item?
+}
+
+// Add Todo button
+form.addEventListener("submit", function (e) {
+  e.preventDefault();
+  console.log(input.value);
+
+  let newTodo = document.createElement("li");
+  //const removeBtn = document.createElement("button");
+  //const completeBtn = document.createElement("button");
+  removeBtn.innerText = "Remove";
+  completeBtn.innerText = "Completed";
+  newTodo.classList.add("todo");
+  newTodo.innerText = input.value;
+  newTodo.isCompleted = false;
+  // save to localStorage
+  savedTodos.push({ todoItem: newTodo.innerText, isCompleted: false });
+  localStorage.setItem("todosAll", JSON.stringify(savedTodos));
+
+  newTodo.appendChild(removeBtn);
+  newTodo.appendChild(completeBtn);
+  input.value = "";
+  todoList.appendChild(newTodo);
+});
+
+// Remove and Completed button
+todoList.addEventListener("click", function (e) {
+  console.log(e.target.tagName); //BUTTON
+  console.log(e.target.innerText); //Remove / Completed
+  /*
+  if (e.target.tagName === "BUTTON")
+    removeBtn.addEventListener("click", function (e) {
+    e.target.parentElement.remove();
+  });
+  */
+  if (e.target.innerText === "Remove") {
+    e.target.parentElement.remove();
+    //localStorage.removeItem("todos"); => How to link this effect to Remove button?
+  } else if (e.target.innerText === "Completed") {
+    e.target.parentElement.classList.toggle("completed");
+    e.target.isCompleted = true;
+    console.log(e.target.isCompleted); //true
+    //e.target.innerText = "Undo"; => How to toggle Undo & Completed?
+  }
+});
+
+/*const removeButtons = document.querySelectorAll("li button");
+for (let btn of removeButtons) {
+  btn.addEventListener("click", function (e) {
+    e.target.parentElement.remove();
+  });
+}
+*/
+
+// Change background color
+colorSection.addEventListener("click", function (e) {
+  document.body.style.backgroundColor = e.target.dataset.hex;
+});
+
+//localStorage.removeItem("todosAll");
