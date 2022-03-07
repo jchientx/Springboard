e86b35756ea61e50536998193b12711280acef07
jchientx <73362100+jchientx@users.noreply.github.com>
2022-03-06 21:49:42 -0600

    Add files via upload

---
+++ js-Object-Enhancements-Exercise.js
@@ -0,0 +1,41 @@
+// Same keys and values ES2015
+function createInstructor(firstName, lastName) {
+  return {
+    firstName,
+    lastName,
+  };
+}
+
+console.log(createInstructor("James", "Hardon"));
+
+// Computed Property Names ES2015
+let favoriteNumber = 42;
+
+const instructor = {
+  firstName: "Colt",
+  [favoriteNumber]: "That is my favorite!",
+};
+
+// Object Methods ES2015
+const instructor = {
+  firstName: "Colt",
+  sayHi() {
+    return "Hi!";
+  },
+  sayBye() {
+    return this.firstName + "says bye!";
+  },
+};
+// createAnimal function
+function createAnimal(species, verb, noise) {
+  return {
+    species,
+    [verb]() {
+      return noise;
+    },
+  };
+}
+
+const d = createAnimal("dog", "bark", "Woooof!");
+console.log(d); // { species: 'dog', bark: [Function: bark] }
+console.log(d.bark()); // Woooof!
