b4d188d73a48687e62911c31d76e099c67895cfa
jchientx <73362100+jchientx@users.noreply.github.com>
2022-06-26 18:03:54 -0500

    Update app.py

--- flask-survey/app.py
+++ flask-survey/app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template, request, redirect, flash
+from flask import Flask, render_template, request, redirect, flash, session
 from flask_debugtoolbar import DebugToolbarExtension
 from surveys import satisfaction_survey as survey
 
@@ -7,17 +7,22 @@ app.config['SECRET_KEY'] = "secret-survey"
 app.config['DEBUG_TB_INTERCEPT_REDIRECTS'] = False
 debug = DebugToolbarExtension(app)
 
-responses = []
+#responses = []
+session_key = "responses"
 
 @app.route("/")
 def start_page():
-    #title = survey.title
-    #instructions = survey.instructions
-    return render_template("start_page.html", survey=survey) #title=title, instructions=instructions
+    return render_template("start_page.html", survey=survey) 
+
+@app.route("/begin", methods=["POST"])
+def begin():
+    session[session_key] = []
+    return redirect("/questions/0")
 
 @app.route("/questions/<int:num>")
 def questions(num):
     
+    responses = session.get(session_key)
     # Step Six: Protecting Questions
     # if the user is trying to access question page, go to the start page
     if responses is None:
@@ -36,9 +41,13 @@ def questions(num):
 
 @app.route("/answer", methods=["POST"])
 def handle_answers():
+    # get the response choice
     answer = request.form["answer"]
-
+    
+    # add this response to the session
+    responses = session[session_key]
     responses.append(answer)
+    session[session_key] = responses
 
     if len(responses) == len(survey.questions):
         return redirect("/thanks")
